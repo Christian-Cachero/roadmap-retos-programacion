0731ee5ac7a2bb85b9d3bbe0e37591542a14c5b9
Christian Cachero <Ch_canchero@live.com.ar>
2025-04-29 00:19:06 -0300

    ¡Primer reto listo! Mi cuanta de gitHub es Christian-Cachero, pero el "-" no me lo acepta así que lo dejaré así.

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/ChristianCachero.java
@@ -0,0 +1,39 @@
+public class ChristianCachero {
+
+    //https://www.oracle.com/java/
+
+    //Diferentes sintaxis al escribir un comentario:
+
+    // <- esto es un comentario de una línea
+
+    /*esto es
+     * un comentario
+     * de varias líneas.*/
+
+    /**
+     * Esto es un javadoc*/
+
+    //Variables y constantes:
+
+    //Enteros:
+    byte estoEsUnByte = 1;
+    short estoEsUnShort = 1;
+    long estoEsUnLong = 1;
+    int estoEsUnEntero = 1;
+
+    //punto flotante:
+    float estoEsUnDecimal = 1.5f;
+    double estoEsUnDecimalMasPreciso = 1.5;
+
+    //carateres/cadena de caracteres y booleanos:
+    char estoEsUnCaracter= 'a';
+    String estoEsUnaCadenaDeCaracteres = "hola";
+    boolean estoEsUnBooleano = true; // false
+
+    //constantes:
+    final String getEstoEsUnaCadenaDeCaracteresCostante = "SOY_UNA_CONSTANTE";
+
+    public static void main(String[] args){
+        System.out.println("!Hola, Java!");
+    }
+}
